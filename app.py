0e23814cc22c5275de4fbec945f9a9b894169895
Rutuj Satpute <satputerutuj0070gmail.com>
2026-03-05 20:00:05 +1000

    Upgraded app to version 2.0

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     # This is the message users will see in their browser
-    return "<h1>DevOps Project v1.0</h1><p>The application is running successfully!</p>"
+    return "This app is working successfully on My laptop"
 
 if __name__ == '__main__':
     # We run the app on port 5000
